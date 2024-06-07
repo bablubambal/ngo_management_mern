084eee56c6cc860c3398f764a57dfc6fa0358d63
bablubambal <bablubambal1999@gmail.com>
2024-06-07 22:45:16 +0530

    create NGO

--- frontend/src/components/CreateNGO.js
+++ frontend/src/components/CreateNGO.js
@@ -24,11 +24,16 @@ const CreateNGO = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="name" value={name} onChange={onChange} placeholder="NGO Name" required />
-      <textarea name="description" value={description} onChange={onChange} placeholder="Description" required />
-      <button type="submit">Create NGO</button>
+    <>
+   <div className='container m-5 d-flex justify-content-center   '>
+   <form onSubmit={onSubmit} style={{width:'80%'}}>
+    <h2> Create Your NGO</h2>
+      <input className='form-control mt-2' type="text" name="name" value={name} onChange={onChange} placeholder="NGO Name" required />
+      <textarea className='form-control mt-2' name="description" value={description} onChange={onChange} placeholder="Description" required />
+      <button className='form-control mt-2 btn btn-danger' type="submit">Create NGO</button>
     </form>
+   </div>
+    </>
   );
 };
 
